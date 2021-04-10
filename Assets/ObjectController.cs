1a628b05b58a6d19f3faaba75ce8c57bdc352f59
m.kato.mentor <hakusui25@gmail.com>
2021-04-10 15:38:46 +0900

    Initial commit

---
+++ Assets/ObjectController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectController : MonoBehaviour
+{
+    // カメラの位置
+    private GameObject camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.camera = GameObject.Find("Main Camera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 表示範囲を超えたオブジェクトを破棄
+        if (this.transform.position.z < this.camera.transform.position.z)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+}
